807d8dd220c109845ceac93bd2c8fb5f64889bbf
yangtianyi <yangtianyi@xiaomi.com>
2018-08-28 20:23:13 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('my-cli start');
+console.log('my-cli running...');
+
+var path = require('path');
+
+console.log("argv:", process.argv);
+var arguments = process.argv.splice(2);
+console.log("arguments:", arguments);
